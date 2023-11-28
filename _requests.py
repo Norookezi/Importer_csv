8881a372ac8f234f4b04578eb78c96899a306a2c
Norookezi <norookezi@pandacrp.com>
2023-11-28 11:59:12 +0100

    feat: :sparkles: Created _requests file
    This fill will soon contain all codes to send to sql

---
+++ _requests.py
@@ -0,0 +1,21 @@
+class Request:
+    def add_or_cancel(self)-> None:
+        raise NotImplementedError("Method not implemented yet")
+
+    def add_or_pass(self)-> None:
+        raise NotImplementedError("Method not implemented yet")
+
+    def add_or_duplicate(self)-> None:
+        raise NotImplementedError("Method not implemented yet")
+
+    def add_or_replace(self)-> None:
+        raise NotImplementedError("Method not implemented yet")
+
+    def update_or_pass(self)-> None:
+        raise NotImplementedError("Method not implemented yet")
+
+    def update_or_cancel(self)-> None:
+        raise NotImplementedError("Method not implemented yet")
+
+    def update_or_add(self)-> None:
+        raise NotImplementedError("Method not implemented yet")
