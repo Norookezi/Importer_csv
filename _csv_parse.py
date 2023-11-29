ad4d5d338471a64ad326dbd1f2430c4ac606bd92
Norookezi <norookezi@pandacrp.com>
2023-11-29 16:48:16 +0100

    feat: :sparkles: Added field to Cvs parse
    Added a fields attribute in Csv_parse class

--- _csv_parse.py
+++ _csv_parse.py
@@ -1,9 +1,9 @@
 from typing import List
-from _config import Config
+from _config import Config, Field
 
 class Line:
     __values__ = {}
-    
+
     def parse(self, headers, values):
         for index, value in enumerate(values):
             setattr(self, headers[index], value)
@@ -15,26 +15,34 @@ class Csv_parse:
     __rule__: Config = None
     __header__: List[str] = []
     __lines__: List[Line] = []
-    
-    def __init__(self, path: str = None, separator: str = ";"):
+    __fields__: List[Field] = []
+
+    def __init__(self, path: str = None, separator: str = ";", rule: List[Field] = None):
         self.__separator__ = separator
         self.__file__ = path
-        
+        self.__fields__ = rule
+
         self.get_content()
-        
+
     def get_content(self):
         file = open(self.__file__, 'r', encoding='utf-8')
         content = file.read().split('\n')
         self.__header__ = content[0].split(self.__separator__)
-        
+
         self.set_lines(content[1:])
-        
+
     def set_lines(self, lines):
         self.__lines__ = []
+
         for line in lines:
-            lineInstance = Line()
-            test = lineInstance.parse(self.__header__, line.split(self.__separator__))
-            self.__lines__.append(test)
-            
+            line_ = Line()
+
+            self.__lines__.append(line_.parse(self.__header__, line.split(self.__separator__)))
+
+        mandatory_fields = [field for field in self.__fields__ if field.__mandatory__ and not field.__name__ in self.__header__]
+        
+        if len(mandatory_fields) != 0:
+            raise ValueError("Missing fields: %s" % "; ".join([missing_fields.name for missing_fields in mandatory_fields]))
+
 if __name__ == '__main__':
     print(Csv_parse('./test.csv').__dict__)
