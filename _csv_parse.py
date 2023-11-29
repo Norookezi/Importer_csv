486126f2e3d26c69a032d4320bd73e2a93633036
Norookezi <norookezi@pandacrp.com>
2023-11-29 10:53:44 +0100

    fix: :bug: Fixed Csv_parse lines
    Csv_parse wasn't returning lines

--- _csv_parse.py
+++ _csv_parse.py
@@ -2,16 +2,19 @@ from typing import List
 from _config import Config
 
 class Line:
-    def __init__(self, headers, values):
+    __values__ = {}
+    
+    def parse(self, headers, values):
         for index, value in enumerate(values):
             setattr(self, headers[index], value)
+        return (self)
 
 class Csv_parse:
     __separator__ = None
     __file__ = None
     __rule__: Config = None
-    header = []
-    lines: List[Line] = []
+    __header__: List[str] = []
+    __lines__: List[Line] = []
     
     def __init__(self, path: str = None, separator: str = ";"):
         self.__separator__ = separator
@@ -22,14 +25,16 @@ class Csv_parse:
     def get_content(self):
         file = open(self.__file__, 'r', encoding='utf-8')
         content = file.read().split('\n')
-        self.header = content[0].split(self.__separator__)
+        self.__header__ = content[0].split(self.__separator__)
+        
         self.set_lines(content[1:])
         
-        print(self.lines)
-        
     def set_lines(self, lines):
+        self.__lines__ = []
         for line in lines:
-            self.lines.append(Line(self.header, line.split(self.__separator__)))
+            lineInstance = Line()
+            test = lineInstance.parse(self.__header__, line.split(self.__separator__))
+            self.__lines__.append(test)
             
 if __name__ == '__main__':
-    Csv_parse('./test.csv')
+    print(Csv_parse('./test.csv').__dict__)
