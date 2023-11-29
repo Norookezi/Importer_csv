bae094104d83a85ef66772653ce05b272602d16b
Norookezi <norookezi@pandacrp.com>
2023-11-29 16:46:28 +0100

    feat: :sparkles: Added 'Add_or_pass' method
    Created a method to add all lines an if one of them already exist, don't upload it (no link to db yet)

--- _requests.py
+++ _requests.py
@@ -1,21 +1,53 @@
+from _config import Config
+from _csv_parse import Csv_parse
+
 class Request:
-    def add_or_cancel(self)-> None:
+    request_poll = []
+    def insert(self, rule: Config = None, file: Csv_parse = None):
+
+        method = getattr(self, rule.method)
+
+        method(self, rule, file)
+
+    def add_or_cancel(self, rule: Config, file: Csv_parse)-> None:
         raise NotImplementedError("Method not implemented yet")
 
-    def add_or_pass(self)-> None:
+    def add_or_pass(self, rule: Config, file: Csv_parse)-> None:
+        unique_fields = [field for field in rule.fields if field.unique]
+        print([field for field in rule.fields])
+        for line in file.__lines__:
+            request = """
+            INSERT INTO {database}{table}({fields})
+            SELECT {couple_field_value}
+            WHERE
+            NOT EXISTS (
+            	SELECT {fields}
+            	FROM {database}{table}
+            	WHERE {condition}
+            );
+            """.format(
+                    database=rule.database,
+                    table=rule.table,
+                    fields=", ".join([field.field for field in rule.fields]),
+                    couple_field_value = ", ".join(
+                        ["{field} = \"{value}\"".format(value=getattr(line, fields.name), field=fields.field) for fields in rule.fields]),
+                    condition = " or ".join(["{field} = \"{value}\"".format(value=getattr(line, fields.name), field=fields.field) for fields in rule.fields if fields.unique])
+                    )
+            self.request_poll.append(request)
+        
+        print("\n\n\n\n".join(self.request_poll))
+
+    def add_or_duplicate(self, rule: Config, file: Csv_parse)-> None:
         raise NotImplementedError("Method not implemented yet")
 
-    def add_or_duplicate(self)-> None:
+    def add_or_replace(self, rule: Config, file: Csv_parse)-> None:
         raise NotImplementedError("Method not implemented yet")
 
-    def add_or_replace(self)-> None:
+    def update_or_pass(self, rule: Config, file: Csv_parse)-> None:
         raise NotImplementedError("Method not implemented yet")
 
-    def update_or_pass(self)-> None:
+    def update_or_cancel(self, rule: Config, file: Csv_parse)-> None:
         raise NotImplementedError("Method not implemented yet")
 
-    def update_or_cancel(self)-> None:
-        raise NotImplementedError("Method not implemented yet")
-
-    def update_or_add(self)-> None:
+    def update_or_add(self, rule: Config, file: Csv_parse)-> None:
         raise NotImplementedError("Method not implemented yet")
